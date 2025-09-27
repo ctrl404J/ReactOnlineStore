7354c76a61497edb61cda6da9cd3c904ae66de92
Jo_ <2vwatt@gmail.com>
2025-09-27 23:53:41 +0900

    useNavigate, nested routes, Outlet의 사용

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import './App.css'
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import data from './data.jsx';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
+import Detail from './pages/Detail.jsx'
 
 function App() {
   let [itemData, setData] = useState(data);
   let copy = [...itemData];
+  let navigate = useNavigate(); //1. useNavigate의 사용법
 
   return (
     <div className='App'>
@@ -14,9 +16,9 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">WOWShop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link onClick={()=>{navigate('/')}}>Home</Nav.Link>
             <Nav.Link href="#features">New</Nav.Link>
-            <Nav.Link href="#pricing">Cart</Nav.Link>
+            <Nav.Link onClick={()=>{navigate('/detail')}}>Cart</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
@@ -38,11 +40,15 @@ function App() {
             </div>
           </div>
         } />
-        <Route path='/detail' element={<div>상세페이지</div>} />
+        <Route path='/detail' element={<Detail/>} />
+        {/* nested routes 사용법 */}
+        <Route path='/about' element={<About/>}>  
+          <Route path='member' element={<div>멤버임</div>} />
+          <Route path='location' element={<div>위치정보임</div>} />
+        </Route>
+        
+        <Route path='*' element={<div>없는 페이지입니다.</div>}></Route>
       </Routes>
-
-      <Link to='/'>홈 </Link>
-      <Link to='/detail'>상세페이지</Link>
 
     </div>
   )
@@ -57,6 +63,15 @@ function Item(props) {
       <div>{props.data.price}$</div>
     </li>
   );
+}
+function About(){
+  return (
+    <div>
+      <h4>회사정보</h4>
+      {/* Outlet으로 nested routes하단 페이지의 내용을 원하는 위치에 랜더링 시키기  */}
+      <Outlet></Outlet>
+    </div>
+  )
 }
 
 export default App
---
+++ src/pages/Detail.jsx
@@ -0,0 +1,15 @@
+function Detail() {
+    return (
+        <ul className="item-container">
+            <li className='shop-item'>
+                <img src='/c1.png' alt='No Image'></img>
+                <h4>귀족로브</h4>
+                <div>마법+7</div>
+                <div>22000$</div>
+                <button className="btn btn-danger">주문하기</button>
+            </li>
+        </ul>
+    );
+}
+
+export default Detail;
