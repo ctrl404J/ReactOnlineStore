88a4e40bbbbbef48d76a9597b95ac628d12d3319
Jo_ <2vwatt@gmail.com>
2025-09-29 01:03:03 +0900

    리액트라우터의 useParams훅 사용(URL파라미터 사용)

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,11 +1,17 @@
-function Detail() {
+import { useParams } from "react-router-dom";
+
+function Detail(props) {
+    let {id} = useParams();
+    let item = props.itemData.find(function(propsItem){ //배열에서 하나씩 꺼내는 파람
+        return propsItem.id == id;
+    })
     return (
         <ul className="item-container">
             <li className='shop-item'>
-                <img src='/c1.png' alt='No Image'></img>
-                <h4>귀족로브</h4>
-                <div>마법+7</div>
-                <div>22000$</div>
+                <img src={item.imageUrl} alt='No Image'></img>
+                <h4>{item.title}</h4>
+                <div>{item.content}</div>
+                <div>{item.price}$</div>
                 <button className="btn btn-danger">주문하기</button>
             </li>
         </ul>
